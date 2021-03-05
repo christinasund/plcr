2f9bc487ee2a50d650c1dbde6b4c051a542d8104
christinasund <christina.sund@tealium.com>
2021-03-05 14:02:40 -0800

    Add bitcode to script

--- Scripts/build-framework.sh
+++ Scripts/build-framework.sh
@@ -12,13 +12,13 @@ echo "=== BUILD TARGET $1 OF PROJECT ${PROJECT_NAME} WITH CONFIGURATION ${CONFIG
 if [ "$2" == "maccatalyst" ]; then
     # Mac Catalyst is a special case - "destination" parameter must be used here.
     env -i "PATH=$PATH" xcodebuild \
-        SYMROOT="${SYMROOT}" OBJROOT="${BUILT_PRODUCTS_DIR}" PROJECT_TEMP_DIR="${PROJECT_TEMP_DIR}" \
+        SYMROOT="${SYMROOT}" ENABLE_BITCODE=YES OBJROOT="${BUILT_PRODUCTS_DIR}" PROJECT_TEMP_DIR="${PROJECT_TEMP_DIR}" \
         ONLY_ACTIVE_ARCH=NO \
         -project "${PROJECT_NAME}.xcodeproj" -configuration "${CONFIGURATION}" \
         -scheme "$1" -destination 'platform=macOS,variant=Mac Catalyst'
 else
     env -i "PATH=$PATH" xcodebuild \
-        SYMROOT="${SYMROOT}" OBJROOT="${BUILT_PRODUCTS_DIR}" PROJECT_TEMP_DIR="${PROJECT_TEMP_DIR}" \
+        SYMROOT="${SYMROOT}" ENABLE_BITCODE=YES OBJROOT="${BUILT_PRODUCTS_DIR}" PROJECT_TEMP_DIR="${PROJECT_TEMP_DIR}" \
         ONLY_ACTIVE_ARCH=NO \
         -project "${PROJECT_NAME}.xcodeproj" -configuration "${CONFIGURATION}" \
         -target "$1" -sdk "$2"
